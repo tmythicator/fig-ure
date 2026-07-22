992224c8752ef98288656a2432e7ca5d297e543a
Alexandr Timchenko <atimchenko92@gmail.com>
2026-07-22 12:13:08 +0200

    feat(sensor): add formatting/validator code for sensor telemetry

--- src/fig_ure/sensors.clj
+++ src/fig_ure/sensors.clj
@@ -13,11 +13,18 @@
   (println "Halting sensor reader..." state))
 
 (defn format-reading
+  "Formats a raw sensor reading into the internal telemetry map structure."
   [sensor-id raw-val unit]
   {:sensor/id sensor-id
    :sensor/value raw-val
    :sensor/unit unit
    :sensor/timestamp (System/currentTimeMillis)})
+
+(defn valid-reading?
+  "Check if a reading is valid (number and within reasonable range)."
+  [reading]
+  (let [val (:sensor/value reading)]
+    (boolean (and (number? val) (<= 0 val 100.0)))))
 
 (comment
   ;; Interactive REPL scratchpad
--- test/fig_ure/sensors_test.clj
+++ test/fig_ure/sensors_test.clj
@@ -9,3 +9,21 @@
                  :sensor/value 42.5
                  :sensor/unit  :percent}
                 (sensors/format-reading :soil-moisture 42.5 :percent)))))
+
+(deftest valid-reading-test
+  (testing "validates sensor readings"
+    (let [mock-reading {:sensor/id :soil-moisture
+                        :sensor/unit :percent
+                        :sensor/timestamp (System/currentTimeMillis)}]
+
+      (clojure.test/are [expected value]
+                        (= expected (sensors/valid-reading?
+                                     (assoc mock-reading :sensor/value value)))
+        true 33.1
+        true 0.0
+        true 100.0
+        true 3
+        false -1.0
+        false 101.0
+        false ""
+        false :test))))
